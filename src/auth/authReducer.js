d3a9484584e5fc0d126b96563dbbbfc724f06b06
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2022-03-20 15:10:55 -0500

    rutas publicas y privadas

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -1,6 +1,17 @@
+import { types } from "../types/types";
 
-const authReducer = (state = {},action) =>{
+export const authReducer = (state = {},action) =>{
     switch(action.type){
-        
+        case types.login:
+            return {
+                ...action.payload,
+                logged:true
+            }
+        case types.logout:
+            return {
+                logged:false
+            }
+        default:
+            return state;
     }
 }
--- src/components/iu/Navbar.js
+++ src/components/iu/Navbar.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
 
 export const Navbar = () => {
+const { user,dispatch } = useContext(AuthContext);
 const navigate = useNavigate();
   const handleLogout = () => {
     //TODO: por hacer
-    console.log("logout");
+    const action = {
+      type: types.logout
+    }
+    dispatch(action);
+    //localStorage.removeItem('user');
     navigate("/login",{
       replace:true
     })
   };
-
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -52,7 +58,7 @@ const navigate = useNavigate();
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Juan David Mendez</span>
+          <span className="nav-item nav-link text-info">{user.name}</span>
           <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
           </button>
